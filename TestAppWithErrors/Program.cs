e194ed7ffb144bc87f9211202ef71c52bb764614
Lenovo <juri.golomako@gmail.com>
2020-04-23 13:17:56 +0300

    Load initial files

---
+++ TestAppWithErrors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestAppWithErrors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int x = "abc";
+        }
+
+        public async Task<int> Test ()
+        {
+            return 1;
+        }
+    }
+}
