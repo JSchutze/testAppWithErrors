b5b8e427e203d2b822d15258b977378f1e4ffee1
Lenovo <juri.golomako@gmail.com>
2020-05-06 12:58:22 +0300

    fff

---
+++ TestAppWithErrors/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAppWithErrors
+{
+    private class Class1
+    {
+        public void Do()
+        
+
+    }
+}
